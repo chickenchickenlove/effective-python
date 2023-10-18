659475266f6e08764e45f1d03d50312ba513513f
ChickenchickenLove <ojt90902@naver.com>
2023-10-18 23:38:25 +0900

    effective python item 65 study

---
+++ item65/65-1.py
@@ -0,0 +1,30 @@
+import json
+
+def load_json_key(data, key):
+    try: # 예외가 반드시 발생할 수 있는 부분을 나타냄.
+        print('* JSON 데이터 읽기')
+        result_dict = json.loads(data)
+    except ValueError as e:
+        print('* ValueError 처리')
+        raise KeyError(key) from e # ValueError가 발생할 수 있음.
+    else: # 예외가 발생하지 않는 부분을 나타내거나 /// 여기서 발생하는 예외는 위로 던진다고 명시하고 싶을 때 사용.
+        print('* 키 검색')
+        return result_dict[key] # KeyError가 발생할 수 있음.
+
+
+data = '{"foo": "bar",}'
+key = 'foo'
+
+try:
+    'bar' == load_json_key(data, key)
+except KeyError as e:
+    print(f"Caught an error: {e}")
+    if e.__cause__:
+        print(f"Original cause: {e.__cause__}")
+
+
+
+
+
+
+
