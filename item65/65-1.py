e5838b1c23530025a4c6bddba8652d5d8284928f
ChickenchickenLove <ojt90902@naver.com>
2024-01-13 16:49:01 +0900

    item65 study

--- item65/65-1.py
+++ item65/65-1.py
@@ -7,7 +7,8 @@ def load_json_key(data, key):
     except ValueError as e:
         print('* ValueError 처리')
         raise KeyError(key) from e # ValueError가 발생할 수 있음.
-    else: # 예외가 발생하지 않는 부분을 나타내거나 /// 여기서 발생하는 예외는 위로 던진다고 명시하고 싶을 때 사용.
+    else:
+        # 예외가 발생하지 않는 부분을 나타냄.
         print('* 키 검색')
         return result_dict[key] # KeyError가 발생할 수 있음.
 
@@ -21,10 +22,3 @@ except KeyError as e:
     print(f"Caught an error: {e}")
     if e.__cause__:
         print(f"Original cause: {e.__cause__}")
-
-
-
-
-
-
-
